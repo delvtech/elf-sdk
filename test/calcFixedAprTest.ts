557c4678a2921a2698acbd3bee86191aa2214a67
Sarah Mackey <sarahmackey42@gmail.com>
2021-07-14 12:59:55 -0700

    Add unit test

---
+++ test/calcFixedAprTest.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 Element Finance, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { calcFixedAPR } from "../src/helpers/calcFixedAPR";
+// import {
+//   THIRTY_DAYS_IN_SECONDS,
+//   ONE_YEAR_IN_SECONDS,
+// } from "../src/constants/time";
+
+import { expect } from "chai";
+import { BigNumber } from "ethers";
+
+describe("calcFixedAPR", () => {
+  it("should properly calculate fixed APR", () => {
+    const spotPrice = .9280;
+    const timeUntilMaturity = 89/365;
+    const result = calcFixedAPR(spotPrice, timeUntilMaturity);
+    expect(result).to.equal(7.382022471910118);
+  });
+
+});
