c515f30f7c4ac48e68dc99b9c6a18d4487c0e2d5
dantop114 <antoniodellaporta.13@protonmail.com>
2021-09-14 01:24:53 +0200

    Adds `getOpenTerms` helper method (#33)
    * Adds `getOpenTerms` helper method
    
    * Remove `console.log`
    
    * Fix `style-check`

---
+++ src/helpers/getOpenTerms.ts
@@ -0,0 +1,36 @@
+import { Provider } from "@ethersproject/providers";
+import { TrancheFactory__factory } from "elf-contracts-typechain";
+import { Signer } from "ethers";
+import { getLatestBlockTimestamp } from "./getLatestBlockTimestamp";
+
+/**
+ * Returns an array of not expired tranche addresses.
+ *
+ * @param trancheFactoryAddress The TrancheFactory that deployed the tranches
+ * @returns a promise of an array of tranche addresses
+ */
+
+export async function getOpenTerms(
+  trancheFactoryAddress: string,
+  signerOrProvider: Signer | Provider
+): Promise<string[]> {
+  const trancheFactoryContract = TrancheFactory__factory.connect(
+    trancheFactoryAddress,
+    signerOrProvider
+  );
+
+  const queryFilter = trancheFactoryContract.filters.TrancheCreated(
+    null,
+    null,
+    null
+  );
+
+  const trancheEvents = await trancheFactoryContract.queryFilter(queryFilter);
+  const timestamp = await getLatestBlockTimestamp();
+
+  const notExpiredTranches: string[] = trancheEvents
+    .filter((event) => event.args?.expiration.toNumber() > timestamp)
+    .map((event) => event.args?.trancheAddress);
+
+  return notExpiredTranches;
+}
