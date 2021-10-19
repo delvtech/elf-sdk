040133d5fda83448898596734b3e5c3c31347635
Sarah Mackey <sarahmackey42@gmail.com>
2021-10-19 15:57:29 -0500

    add import, rebuild (#34)

---
+++ dist/src/helpers/getOpenTerms.d.ts
@@ -0,0 +1,12 @@
+import { Provider } from "@ethersproject/providers";
+import { Signer } from "ethers";
+/**
+ * Returns an array of not expired tranche addresses.
+ *
+ * @param trancheFactoryAddress The TrancheFactory that deployed the tranches
+ * @returns a promise of an array of tranche addresses
+ */
+export declare function getOpenTerms(
+  trancheFactoryAddress: string,
+  signerOrProvider: Signer | Provider
+): Promise<string[]>;
