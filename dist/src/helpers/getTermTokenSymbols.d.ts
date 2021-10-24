e04598f03bec80958e516fd38446e330c964e833
jonny rhea <jonathan.rhea@gmail.com>
2021-10-23 22:02:03 -0500

    run prettier

--- dist/src/helpers/getTermTokenSymbols.d.ts
+++ dist/src/helpers/getTermTokenSymbols.d.ts
@@ -1,14 +1,14 @@
 import { Provider } from "@ethersproject/providers";
 import { Signer } from "ethers";
 export interface TermTokenSymbolsResult {
-    /**
-     * name of the Principal Token
-     */
-    principalTokenSymbol: string;
-    /**
-     * name of the Yield Token
-     */
-    yieldTokenSymbol: string;
+  /**
+   * name of the Principal Token
+   */
+  principalTokenSymbol: string;
+  /**
+   * name of the Yield Token
+   */
+  yieldTokenSymbol: string;
 }
 /**
  * returns the token symbols for a given term
@@ -16,4 +16,7 @@ export interface TermTokenSymbolsResult {
  * @param signerOrProvider
  * @returns a promise for a TermTokenSymbolsResult
  */
-export declare function getTermTokenSymbols(termAddress: string, signerOrProvider: Signer | Provider): Promise<TermTokenSymbolsResult>;
+export declare function getTermTokenSymbols(
+  termAddress: string,
+  signerOrProvider: Signer | Provider
+): Promise<TermTokenSymbolsResult>;
--- dist/src/helpers/getUnderlyingContractsByAddress.d.ts
+++ dist/src/helpers/getUnderlyingContractsByAddress.d.ts
@@ -1,10 +1,18 @@
 import { Provider } from "@ethersproject/providers";
 import { Signer } from "ethers";
-import { ERC20, ERC20Permit, WETH, DAI } from "elf-contracts-typechain/dist/types";
+import {
+  ERC20,
+  ERC20Permit,
+  WETH,
+  DAI,
+} from "elf-contracts-typechain/dist/types";
 /**
  * This method creates a token to contract mapping
  * @param addressesJsonId mainnet or goerli
  * @param signerOrProvider
  * @returns a mapping of token addresses to corresponding pool contracts
  */
-export declare function getUnderlyingContractsByAddress(addressesJsonId: string, signerOrProvider: Signer | Provider): Record<string, ERC20 | WETH | DAI | ERC20Permit>;
+export declare function getUnderlyingContractsByAddress(
+  addressesJsonId: string,
+  signerOrProvider: Signer | Provider
+): Record<string, ERC20 | WETH | DAI | ERC20Permit>;
